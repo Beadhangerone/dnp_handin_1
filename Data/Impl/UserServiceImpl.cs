c5e1b4cd4c7f5f4d77c0a0d9375049811d758f04
Beadhangerone <denm.2206@gmail.com>
2020-10-15 16:00:29 +0200

    Users read/write to file

--- Data/Impl/UserServiceImpl.cs
+++ Data/Impl/UserServiceImpl.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using h1.Models;
@@ -9,16 +10,39 @@ namespace h1.Data.Impl
 {
     public class UserServiceImpl : IUserService
     {
-        private List<User> users;
+        private List<User> users = new List<User>();
         private readonly string users_path = "users.json";
-        
+
         public UserServiceImpl()
         {
-            Console.WriteLine("I'm here!");
-            readJSONToList(users_path, users);
+            // TODO Fix double call
+            DBSync();
         }
 
-        private async Task readJSONToList<T>(string path, List<T> list)
+        private async void DBSync()
+        {
+            initFile(users_path);
+            users = readJSONToList<User>(users_path);
+            
+            if (users.Count == 0)
+            {
+                fillUsers();
+                await writeListToJSON(users_path, users);
+            }
+        }
+
+        private async Task initFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                using (FileStream fs = File.Create(path))
+                {
+                    await JsonSerializer.SerializeAsync(fs, new List<String>());
+                }
+            }
+        }
+
+        private List<T> readJSONToList<T>(string path)
         {
             if (File.Exists(path))
             {
@@ -27,16 +51,40 @@ namespace h1.Data.Impl
                 {
                     string json = r.ReadToEnd();
                     items = JsonSerializer.Deserialize<List<T>>(json);
+                    return items;
                 }
-                list = items;
             }
-            else
+
+            throw new Exception("File " + path + " not found!");
+        }
+
+        private async Task writeListToJSON<T>(string path, List<T> list)
+        {
+            if (File.Exists(path))
             {
                 using (FileStream fs = File.Create(path))
                 {
-                    await JsonSerializer.SerializeAsync(fs, new List<String>());
+                    await JsonSerializer.SerializeAsync(fs, list);
                 }
             }
+            else
+            {
+                throw new Exception("file not found!");
+            }
+        }
+
+
+        private void fillUsers()
+        {
+            users = new[]
+            {
+                new User
+                {
+                    UserName = "Troels",
+                    Password = "1234",
+                    SecurityLevel = 3
+                }
+            }.ToList();
         }
 
         public User ValidateUser(string userName, string password)
