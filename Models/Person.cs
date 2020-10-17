bc99fd4891be5ee3c666f7fdb2cfa4376c5bebc5
shark12000 <themrsnipervip@gmail.com>
2020-10-17 16:51:58 +0300

    Add adult

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,11 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace Models {
-public class Person {
-    
+public class Person
+{ 
     public int Id { get; set; }
     [NotNull]
     public string FirstName { get; set; }
